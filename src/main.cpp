0218189a5713dd75d1b3992f165c7be18d5f346c
xue12311 <1712530035@qq.com>
2022-06-19 19:50:07 +0800

    中文字符

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,21 @@
 #include <Arduino.h>
 #include <TFT_eSPI.h>
+#include <China_TFT.h>
+
+
 TFT_eSPI tft = TFT_eSPI();
+
+
 void setup()
 {
     // 初始化
     tft.init();
-    //清理屏幕  设置屏幕背景色
+    tft.setRotation(0);
     tft.fillScreen(TFT_BLACK);
+    tft.setSwapBytes(true);
+    //文字
+    tft.drawBitmap(10, 10, str_1,16, 16, TFT_WHITE);
 }
-
 void loop()
 {
 }
