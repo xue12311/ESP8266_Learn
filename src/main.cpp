6c602ef03f70c40b4c368c36b888d7856b73fc25
xue12311 <1712530035@qq.com>
2022-06-19 11:09:00 +0800

    tft 学习

--- src/main.cpp
+++ src/main.cpp
@@ -1,81 +1,49 @@
 #include <Arduino.h>
+#include <TFT_eSPI.h>
+TFT_eSPI tft = TFT_eSPI();
+void setup()
+{
+    // 初始化
+    tft.init();
+    //设置屏幕背景色
+    tft.fillScreen(TFT_BLACK);
 
-/**
- * 建立基本网络服务器 显示 引脚 D3 状态 , 自动刷新
- */
-#include <ESP8266WiFi.h>
-#include <ESP8266WiFiMulti.h>
-#include <ESP8266WebServer.h>
+    // 设置起始坐标(20, 10)，4 号字体
+    tft.setCursor(20, 10, 4);
+    // 设置字体颜色为白色 , 黑色文本背景
+    tft.setTextColor(TFT_WHITE, TFT_BLACK);
+    // 设置显示的文字，注意这里有个换行符 \n 产生的效果
+    tft.println("White Text \n");
+    tft.println("Black Text");
+    //不换行
+    tft.print("White Text ");
+    tft.print("Black Text");
 
-//按钮引脚 D3
-#define buttonPin D3
+    //设置起始坐标(10, 120)，2号字体，文本颜色为红色，背景色为白色
+    tft.setCursor(10, 120);
+    tft.setTextFont(2);
+    tft.setTextColor(TFT_RED, TFT_WHITE);
+    tft.println("Red Text And White Background");
 
-ESP8266WiFiMulti wifiMulti;
-//建立ESP8266WebServer对象  端口号: 80
-ESP8266WebServer esp8266_server(80);
+    //设置起始坐标(10, 140)，4号字体，文本颜色为绿色，无背景色
+    tft.setCursor(10, 140,4);
+    tft.setTextColor(TFT_GREEN);
+    tft.println("Green Text");
 
-//存储 引脚状态
-bool pinState = false;
+    //设置起始坐标(50, 180)，4号字体，文本颜色为蓝色，无背景色
+    tft.setCursor(50, 180);
+    tft.setTextFont(4);
+    tft.setTextColor(TFT_BLUE);
+    tft.println("2022-08-21");
 
-String sendHTML(bool buttonState) {
-    String html = "<!DOCTYPE html><html><head><meta http-equiv='refresh' content='5'/><meta charset=\"utf-8\"><title>ESP8266</title></head><body>";
-    html += "<h1>ESP8266 Button State Auto Refresh</h1>";
-    html += "<p>按钮状态: ";
-    html += buttonState ? "松开" : "按下";
-    html += "</p>";
-    html += "<p>引脚状态: ";
-    html += buttonState==HIGH ? "高电平" : "低电平";
-    html += "</p>";
-    html += "</body></html>";
-    return html;
-}
-
-void handleRoot() {
-    esp8266_server.send(200, "text/html", sendHTML(pinState));
-}
-
-
-void handleNotFound() {
-    esp8266_server.send(404, "text/plain", "404: Not Found");
-}
-
-void setup() {
-    //启用串口通讯
-    Serial.begin(9600);
-    delay(10);
-    Serial.print("");
-    //将 按键引脚D3 设置为 输入上拉模式
-    pinMode(buttonPin, INPUT_PULLUP);
-    //使用时 需要连接的WiFi
-    wifiMulti.addAP("public void main () {}", "zhangjiaxue");
-    wifiMulti.addAP("EU-education", "eu456987");
-    //连接 WiFi
-    int i = 0;
-    while (wifiMulti.run() != WL_CONNECTED) {
-        delay(1000);
-        Serial.print(i++);
-        Serial.print(".");
-    }
-    Serial.print("wifi 连接成功 \n");
-    Serial.print("WiFi名称:" + String(WiFi.SSID()));
-    Serial.print("IP地址: " + WiFi.localIP().toString());
-
-    //启动网络服务
-    Serial.println("启动网络服务");
-//--------"启动网络服务功能"程序部分开始-------- //
-    esp8266_server.begin();
-    //设置服务器根目录即'/'的函数
-    esp8266_server.on("/", handleRoot);
-    //设置处理404情况的函数
-    esp8266_server.onNotFound(handleNotFound);
-//--------"启动网络服务功能"程序部分结束--------
-    Serial.print("HTTP esp8266_server started\n");
+    //设置起始坐标(50, 240)，7号字体，文本颜色为 粉色，无背景色
+    tft.setCursor(10, 240);
+    tft.setTextFont(7);
+    tft.setTextColor(TFT_PINK);
+    tft.println("20:22:22");
 
 }
 
-void loop() {
-    //处理 Http 服务器访问
-    esp8266_server.handleClient();
-    //获取 引脚 D3 状态
-    pinState = digitalRead(buttonPin);
+void loop()
+{
 }
